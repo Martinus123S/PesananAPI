80b1e9471cfc275c32e168f589154ecb8a0e8da3
martinus <martinussitinjak@yahoo.com>
2021-01-13 15:30:37 +0700

    init

--- connection/connection.js
+++ connection/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb+srv://123martinus:sahabat123!@pesanancl.0fq7m.mongodb.net/pesanan?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://123martinus:sahabat123@pesanancl.0fq7m.mongodb.net/pesanan?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
     console.log("Database is Connect");
 }).catch(err=>{
