e8b25da05b7da6f3fdee00422e2ecf8f8525c33f
martinus <martinussitinjak@yahoo.com>
2021-01-13 17:56:21 +0700

    fix error

--- connection/connection.js
+++ connection/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise
-mongoose.connect("mongodb+srv://123martinus:sahabat123@pesancl.0fq7m.mongodb.net/pesanan?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://123martinus:sahabat123@pesancl-shard-00-00.0fq7m.mongodb.net:27017,pesancl-shard-00-01.0fq7m.mongodb.net:27017,pesancl-shard-00-02.0fq7m.mongodb.net:27017/pesanan?ssl=true&replicaSet=atlas-8ra1bv-shard-0&authSource=admin&retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
     console.log("Database is Connect");
 }).catch(err=>{
--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise
-var server_port =  process.env.PORT || 3000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-mongoose.connect(process.env.MONGO_DB,{useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>{
-    console.log("Database is Connect");
-}).catch(err=>{
-    console.log(err);
-})
-const connection = mongoose.connection;
-connection.once("open",function(){
-    console.log("Server is running on Port: " + server_port);
-})
+require('./connection/connection');
 const cors = require('cors');
 const foodController = require('./controller/foodController');
+
 const express = require('express');
 const {Router} = require('express');
 const router = express.Router();
@@ -29,6 +17,4 @@ router.route('/users')
 app.put('/users/:id',foodController.updateOrder);
 app.use(cors());
 app.use(router);
-app.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+app.listen((process.env.PORT || 3000));
