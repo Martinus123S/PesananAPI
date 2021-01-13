a5b6f061781f23c793d7941f3efcb264b3e9da9c
martinus <martinussitinjak@yahoo.com>
2021-01-13 16:22:18 +0700

    fix error

--- connection/connection.js
+++ connection/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise
 
-mongoose.connect(process.env.MONGO_DB,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_DB,{useNewUrlParser: true, useUnifiedTopology: false})
 .then(()=>{
     console.log("Database is Connect");
 }).catch(err=>{
