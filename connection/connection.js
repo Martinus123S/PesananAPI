6021a4efa65df66c73fd8ea96fa1e198d1c039c1
martinus <martinussitinjak@yahoo.com>
2021-01-13 16:25:27 +0700

    fix error

--- connection/connection.js
+++ connection/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise
-
 mongoose.connect(process.env.MONGO_DB,{useNewUrlParser: true, useUnifiedTopology: false})
 .then(()=>{
     console.log("Database is Connect");
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('./connection/connection');
 const cors = require('cors');
 const foodController = require('./controller/foodController');
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 const express = require('express');
 const {Router} = require('express');
 const router = express.Router();
@@ -16,4 +18,6 @@ router.route('/users')
 app.put('/users/:id',foodController.updateOrder);
 app.use(cors());
 app.use(router);
-app.listen((process.env.PORT || 3000));
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
